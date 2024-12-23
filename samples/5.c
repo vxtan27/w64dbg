412c38148183e350c92366b7a5b46838b3d5b41b
vxtan27 <vxtan27@gmail.com>
2024-12-23 10:33:35 +0700

    Update 5.c

--- samples/5.c
+++ samples/5.c
@@ -1,4 +1,7 @@
-/* Copyright (c) 2024, vxtan27. Licensed under the BSD-3-Clause License. */
+/*
+    Copyright (c) 2024, vxtan27, all rights reserved.
+    Licensed under the BSD-3-Clause.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,9 +11,12 @@ int main(void)
     int numerator = 100;
     int denominator = 0;
 
-    // Use div function
+    // Use div function to compute quotient and remainder
     div_t res = div(numerator, denominator);
 
-    printf("Quotient of 100/0 = %d\n", res.quot);
-    printf("Remainder of 100/0 = %d\n", res.rem);
+    // Output the results (although in case of zero denominator, this code won't execute)
+    printf("Quotient of %d / %d = %d\n", numerator, denominator, res.quot);
+    printf("Remainder of %d / %d = %d\n", numerator, denominator, res.rem);
+
+    return 0;  // Return 0 to indicate successful execution
 }
