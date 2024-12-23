01a97fb4cd2e3698874aa4aa6557fffec977229c
vxtan27 <vxtan27@gmail.com>
2024-12-23 10:33:39 +0700

    Update 7.c

--- samples/7.c
+++ samples/7.c
@@ -1,12 +1,24 @@
-/* Copyright (c) 2024, vxtan27. Licensed under the BSD-3-Clause License. */
+/*
+    Copyright (c) 2024, vxtan27, all rights reserved.
+    Licensed under the BSD-3-Clause.
+*/
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 int main(void)
 {
+    // Define a function pointer type for callback functions
     typedef void(*CallbackFunc)(void);
+
+    // Initialize the function pointer to NULL (invalid callback)
     CallbackFunc invalidCallback = NULL;
-    OutputDebugStringW(L"Accessing DEP\n");
-    invalidCallback(); // Dereference a null function pointer
+
+    // Output a message to the debugger (this will be visible in the debugger's output window)
+    OutputDebugStringW(L"Accessing DEP (Data Execution Prevention)\n");
+
+    // Dereference the null function pointer, causing a crash (undefined behavior)
+    invalidCallback();  // This will lead to a crash as the function pointer is NULL
+
+    return 0;  // Return 0 (although it won't be reached due to the crash)
 }
