060dcb54b1b8ff7ce3270097429337de6967718d
vxtan27 <vxtan27@gmail.com>
2024-12-23 10:33:21 +0700

    Update 1.c

--- samples/1.c
+++ samples/1.c
@@ -1,4 +1,7 @@
-/* Copyright (c) 2024, vxtan27. Licensed under the BSD-3-Clause License. */
+/*
+    Copyright (c) 2024, vxtan27, all rights reserved.
+    Licensed under the BSD-3-Clause.
+*/
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
@@ -7,14 +10,42 @@
 int main(void)
 {
     SYSTEM_INFO si;
+    
+    // Retrieve system information (specifically page size)
     GetSystemInfo(&si);
 
     DWORD oldProtect;
     LPVOID guardPage;
-    guardPage = VirtualAlloc(NULL, si.dwPageSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    VirtualProtect(guardPage, si.dwPageSize, PAGE_GUARD | PAGE_READWRITE, &oldProtect);
+
+    // Allocate memory with page size of the system
+    guardPage = VirtualAlloc(
+        NULL,                       // Address of the memory to allocate
+        si.dwPageSize,              // Size of the page to allocate
+        MEM_COMMIT | MEM_RESERVE,    // Allocation type (commit and reserve)
+        PAGE_READWRITE              // Memory protection (read/write)
+    );
+
+    if (guardPage == NULL) {
+        // Check if memory allocation failed
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    // Set the allocated page as a guard page
+    VirtualProtect(
+        guardPage,                  // Address of the memory page
+        si.dwPageSize,              // Size of the page
+        PAGE_GUARD | PAGE_READWRITE, // Protection flags (guard and read/write)
+        &oldProtect                 // Store the old protection settings
+    );
 
     printf("Accessing memory page...\n");
-    // Cause guard page exception
-    *(volatile char *) guardPage = 1;
+
+    // Attempt to access the guard page (this will cause a guard page exception)
+    *(volatile char *)guardPage = 1;
+
+    // The program should not reach here due to the exception being triggered
+    printf("This will not be printed unless guard page exception is handled.\n");
+
+    return 0;
 }
