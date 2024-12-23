bb3e8ec8e73f5fe10c8a7e668d9b1267796b0c45
vxtan27 <vxtan27@gmail.com>
2024-12-23 10:33:43 +0700

    Update 8.cpp

--- samples/8.cpp
+++ samples/8.cpp
@@ -1,4 +1,7 @@
-/* Copyright (c) 2024, vxtan27. Licensed under the BSD-3-Clause License. */
+/*
+    Copyright (c) 2024, vxtan27, all rights reserved.
+    Licensed under the BSD-3-Clause.
+*/
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
@@ -6,21 +9,28 @@
 class Example
 {
 public:
-    // Start the recursion
+    // Public method to initiate recursion
     void CauseException(void)
     {
-        InfiniteRecursive();
+        InfiniteRecursive();  // Start the infinite recursion
     }
+
 private:
+    // Private method that recursively calls itself
     void InfiniteRecursive(void)
     {
-        // Cause stack overflow
+        // Cause a stack overflow by calling the method indefinitely
         InfiniteRecursive();
     }
 };
 
 int main(void)
 {
+    // Create an instance of the Example class
     Example example;
+
+    // Trigger the recursive method that will eventually cause a stack overflow
     example.CauseException();
+
+    return 0;  // Return 0 to indicate successful execution (although this won't be reached due to the stack overflow)
 }
