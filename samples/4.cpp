6d06a17e54a43df84e0dd3085041f9142621b274
vxtan27 <vxtan27@gmail.com>
2024-12-23 10:33:32 +0700

    Update 4.cpp

--- samples/4.cpp
+++ samples/4.cpp
@@ -1,24 +1,44 @@
-/* Copyright (c) 2024, vxtan27. Licensed under the BSD-3-Clause License. */
+/*
+    Copyright (c) 2024, vxtan27, all rights reserved.
+    Licensed under the BSD-3-Clause.
+*/
 
 #include <iostream>
+#include <cstdio>
 
 class Example
 {
 public:
+    // Public method to trigger an exception
     void CauseException(void)
     {
+        // Calling the private method with specific parameters
         RootException(4, 5.6f);
     }
+
 private:
+    // Static inline method that intentionally causes an access violation
     static inline void RootException(int i, double j)
     {
-        // Cause access violation
+        // Print the received parameters (for debugging, if necessary)
+        std::cout << "Received values: i = " << i << ", j = " << j << std::endl;
+
+        // Cause an access violation by incorrectly using std::sscanf
+        // Attempting to dereference an invalid pointer (reinterpret_cast)
         std::sscanf("12345", "%d", reinterpret_cast<int *>(i));
     }
 };
 
-int main(void) {
+int main(void)
+{
+    // Create an instance of Example
     Example example;
+
+    // Notify that we are about to cause an access violation
     std::cout << "Accessing invalid memory...\n";
+
+    // Call the method that triggers the access violation
     example.CauseException();
+
+    return 0;  // Return 0 to indicate successful execution (although it won't be reached)
 }
