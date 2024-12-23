934f79c666c6b62e8b7e91c1560b2e9f6c2e0f53
vxtan27 <vxtan27@gmail.com>
2024-12-23 10:33:37 +0700

    Update 6.cpp

--- samples/6.cpp
+++ samples/6.cpp
@@ -1,4 +1,7 @@
-/* Copyright (c) 2024, vxtan27. Licensed under the BSD-3-Clause License. */
+/*
+    Copyright (c) 2024, vxtan27, all rights reserved.
+    Licensed under the BSD-3-Clause.
+*/
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
@@ -7,20 +10,31 @@
 class Example
 {
 public:
+    // Public method to trigger an exception
     void CauseException(void)
     {
-        RootException();
+        RootException();  // Call the private method to cause a breakpoint
     }
+
 private:
+    // Static inline method that triggers a breakpoint in the debugger
     static inline void RootException(void)
     {
-        // Cause breakpoint
+        // Cause a breakpoint (used for debugging purposes)
         DebugBreak();
     }
 };
 
-int main(void) {
+int main(void)
+{
+    // Create an instance of the Example class
     Example example;
+
+    // Call the method that will trigger a breakpoint
     example.CauseException();
+
+    // Output a message to the debugger (this will be visible in the debugger's output window)
     OutputDebugStringW(L"Ignored breakpoint\n");
+
+    return 0;  // Return 0 to indicate successful execution
 }
