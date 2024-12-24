f8058c2216515c141801051e094f0865e9390326
vxtan27 <vxtan27@gmail.com>
2024-12-24 22:34:37 +0700

    Update license.c

--- src/license.c
+++ src/license.c
@@ -5,6 +5,6 @@
 
 #pragma section(".lic", read, discard)
 __declspec(allocate(".lic"))
-char license[31] = "Licensed under the BSD-3-Clause";
+const char license[31] = "Licensed under the BSD-3-Clause";
 
 #pragma comment(linker, "/INCLUDE:license")
